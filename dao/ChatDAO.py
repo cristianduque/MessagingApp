ac86ec31df9cbe0dcb95a5c146ff233b5f4ffc2a
kristalys47 <kristalys.cba4@gmail.com>
2018-03-27 00:42:02 -0400

    All DAOS' DONE
    Still need to do some checks

--- dao/ChatDAO.py
+++ dao/ChatDAO.py
@@ -2,10 +2,10 @@ from dao.UserDAO import UserDAO
 class ChatDAO:
     def __init__(self):
 
-        C1 = [45, 'RUM', 'Cristian Duque', 123]
-        C2 = [105, 'GrupoICOM5016', 'Cristian Duque', 123]
-        C3 = [500, 'Los Guerrilleros', 'Kristalys Ruiz', 456]
-        C4 = [55, 'Family', 'Gladymar Oneill', 78]
+        C1 = [45, 'RUM', 123]
+        C2 = [105, 'GrupoICOM5016', 123]
+        C3 = [500, 'Los Guerrilleros', 456]
+        C4 = [55, 'Family', 78]
 
         dao = UserDAO()
         self.userData = dao.data
@@ -20,36 +20,36 @@ class ChatDAO:
         return self.dataChats
 
     def getAllOwners(self):
-        return self.dataChats
+        owner = []
+        for o in self.dataChats:
+            owner.append(o)
+
 
     def getAllUsersInChat(self, cid):
+        result = []
         if cid == 45:
-            result = []
             result.append(self.userData[0])
             result.append(self.userData[1])
             result.append(self.userData[2])
             result.append(self.userData[3])
             return result
         elif cid == 105:
-            result = []
             result.append(self.userData[0])
             result.append(self.userData[1])
             result.append(self.userData[2])
             return result
         elif cid == 500:
-            result = []
             result.append(self.userData[1])
             result.append(self.userData[0])
             result.append(self.userData[3])
             return result
         elif cid == 55:
-            result = []
             result.append(self.userData[2])
             result.append(self.userData[1])
             result.append(self.userData[3])
             return result
         else:
-            return None
+            return result
 
 
 
