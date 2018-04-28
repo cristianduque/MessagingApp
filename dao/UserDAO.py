11ef94fc9798b98f6d913ff691066c9c7ff89fe8
cristianduque <cristian.duque@upr.edu>
2018-04-28 09:59:24 -0400

    Eliminated hardwired code on users dao

--- dao/UserDAO.py
+++ dao/UserDAO.py
@@ -4,19 +4,7 @@ from config.dbconfig import pg_config
 
 class UserDAO:
     def __init__(self):
-        U1 = [123, 'Cristian', 'Duque', 7872015817, 'cduque@upr.edu']
-        U2 = [456, 'Kristalys', 'Ruiz', 7874292951, 'kruiz@upr.edu']
-        U3 = [78, 'Gladymar', 'Oneill', 7876624695, 'goneill@upr.edu']
-        U4 = [910, 'Javier', 'Correa', 7878901234, 'j.correa@upr.edu']
-
-        self.messages = MessageDAO().allMessages()
-
-        self.data = []
-        self.data.append(U1)
-        self.data.append(U2)
-        self.data.append(U3)
-        self.data.append(U4)
-
+       
         connection_url = "dbname=%s user=%s password=%s host=%s port=%s" % (pg_config['dbname'], pg_config['user'], pg_config['password'], pg_config['host'], pg_config['port'])
         self.conn = psycopg2.connect(connection_url)
 
