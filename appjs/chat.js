f7f52a6f02aeacd4530d4ca14a9ab8428609788d
kristalys47 <kristalys.cba4@gmail.com>
2018-05-01 02:03:41 -0400

    Message in the UI

--- appjs/chat.js
+++ appjs/chat.js
@@ -2,16 +2,31 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
     function($http, $log, $scope) {
         var thisCtrl = this;
 
+
+        this.msgHW = [];
+        //   0MID, 1TEXT, 2AUTHOR, 3LIKE, 4DISLIKE
+        var m1 = [1, "Holita", "Gladymar", 7, 10];
+        var m2 = [2, "ADIOSSSS", "Cristian", 10, 5];
+        var m3 = [3, "JAJAJAJA", "Kristalys", 0, 4];
+        var m4 = [4, "Hola Mi Amigo", "Bob", 4, 1];
+        var m5 = [5, "Hello World", "Joe", 11, 12];
+
+        thisCtrl.msgHW.push(m5);
+        thisCtrl.msgHW.push(m4);
+        thisCtrl.msgHW.push(m3);
+        thisCtrl.msgHW.push(m2);
+        thisCtrl.msgHW.push(m1);
+
         this.messageList = [];
-        this.counter  = 2;
         this.newText = "";
 
         this.loadMessages = function(){
             // Get the messages from the server through the rest api
-            thisCtrl.messageList.push({"id": 1, "text": "Hola Mi Amigo", "author" : "Bob",
-            "like" : 4, "nolike" : 1});
-            thisCtrl.messageList.push({"id": 2, "text": "Hello World", "author": "Joe",
-                "like" : 11, "nolike" : 12});
+            var n=thisCtrl.msgHW.length;
+            for(var i=0; i<n; i++){
+                var m = thisCtrl.msgHW.pop();
+                thisCtrl.messageList.unshift({"id": m[0], "text": m[1], "author": m[2], "like": m[3], "nolike": m[4]});
+            }
 
             $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
         };
@@ -20,10 +35,11 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             var msg = thisCtrl.newText;
             // Need to figure out who I am
             var author = "Me";
-            var nextId = thisCtrl.counter++;
-            thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "author" : author, "like" : 0, "nolike" : 0});
+            var id= thisCtrl.messageList.length+1;
+            thisCtrl.msgHW.unshift([id, msg, author, 0, 0]);
             thisCtrl.newText = "";
+            this.loadMessages();
         };
 
         this.loadMessages();
-}]);
+    }]);
