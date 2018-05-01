03908d44aac29043cf07dcf18f40bcb91abe0079
cristianduque <cristian.duque@upr.edu>
2018-04-30 22:16:36 -0400

    ContactList routes done

--- handler/ContactListHandler.py
+++ handler/ContactListHandler.py
@@ -4,28 +4,27 @@ from flask import jsonify
 class ContactListHandler:
 
     def maptoDicContactList(self, cl):
-        mapped= {'ContactListID': cl[0], 'OwnerID': cl[1]}
+        mapped= {'uid': cl[0], 'contactid': cl[1]}
         return mapped
 
     def mapToDict(self, row):
-        result = {'uid': row[0], 'first_name': row[1], 'last_name': row[2], 'phone': row[3], 'email': row[4]}
+        result = {'contactid': row[0]}
         return result
 
     def getAllContactLists(self):
         contactList = ContactListDAO().allContactLists()
-        if contactList == None:
+        if not contactList:
             return jsonify(Error="NOT FOUND"), 404
         result = []
         for cl in contactList:
             result.append(self.maptoDicContactList(cl))
         return jsonify(AllContactList=result)
 
-    def getUsersInContactList(self, uid):
+    def getAllContactsFromUser(self, uid):
         users = ContactListDAO().contactlistofUser(uid)
-        if users == None:
+        if not users:
             return jsonify(Error="NOT FOUND"), 404
         result = []
         for u in users:
             result.append(self.mapToDict(u))
         return jsonify(AllUsersInCOntactList=result)
-
