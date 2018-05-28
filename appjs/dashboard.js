9f04c55b083ccf49cc7331bce3169b7cde981cb2
kristalys47 <kristalys.cba4@gmail.com>
2018-05-28 04:30:13 -0400

    trabajando con el dashboard

---
+++ appjs/dashboard.js
@@ -0,0 +1,59 @@
+// Load the Visualization API and the piechart package.
+google.charts.load('current', {'packages': ['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+function reformatData(jsonData){
+    var temp= jsonData.Dashboard;
+    console.log("temp: " + JSON.stringify(temp));
+    var result = [];
+    var i;
+    var row;
+    for (i=0; i < temp.length; ++i){
+        row= temp[i]
+        dataElement = [];
+        dataElement.push(row.id + '-' + row.name);
+        dataElement.push(row.count);
+        result.push(dataElement);
+    }
+    console.log("Data: " + JSON.stringify(result));
+    return result;
+}
+
+function drawChart() {
+    var jsonData = $.ajax({
+        url: "http://localhost:5000/SocialMessagingApp/dashboard",
+        dataType: "json",
+        async: false
+    }).responseText;
+
+    console.log("jsonData: " + JSON.parse(jsonData));
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Parts');
+    data.addColumn('number', 'Stock');
+    data.addRows(reformatData(JSON.parse(jsonData)));
+
+    var options = {
+        title: 'Stock Parts by Id/name',
+        chartArea: {width: '50%'},
+        hAxis: {
+            title: 'Total Number',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Part'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+}
+
+
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawChart);
